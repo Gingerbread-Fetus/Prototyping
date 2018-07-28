be04ca29d5ae12cb8d4792a10f00a5cb9473ab4e
Hayden Matthews <haydenamatthews@yahoo.com>
2018-07-28 01:17:57 -0600

    Revert "Revert "Initial commit of prototyping project in Unity""
    This reverts commit 4a5499f90d86807ab3b49f4ae6a2946605d5b2dc.

---
+++ Prototyping/Assets/Scripts/EnemyCollisionEncounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EnemyCollisionEncounter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //TODO: Play scene header animation
+
+            //Load the battle scene
+            //SceneManager.LoadScene(1);
+        }
+    }
+}
